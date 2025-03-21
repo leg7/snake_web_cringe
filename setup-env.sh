337fa8a2c610cf83ecd6da26f4a41fd531564e8d
leg7 <dev@leonardgomez.xyz>
2025-03-21 11:53:26 +0100

    Fix bad env var and add alias to deploy

--- setup-env.sh
+++ setup-env.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-export TOMCAT_WEBAPP_DIR="$(readlink -f apache-tomcat-11.0.5/webapps/app)"
+TOMCAT_WEBAPP_DIR="$(readlink -f apache-tomcat-11.0.5/webapps)"
+export TOMCAT_WEBAPP_DIR
+
+alias gd="gradle deploy --no-configuration-cache"
